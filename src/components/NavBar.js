849a40cfbf1fa582c8e8c07f0d59a52962cd070f
mariesonko <mariesonko62@gmail.com>
2018-07-16 17:43:12 -0400

    Fix NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,9 @@ class NavBar extends React.Component {
  render() {
    return (
      <div>
-       <Navbar brand='Optima' center='true' className="nav">
+        <Navbar brand='OPTIMA' center='true' className="nav1 brand-logo ">
+          </Navbar>
+       <Navbar brand='Optima' center='true' className="nav brand-logo ">
          <NavItem href="/">Home</NavItem>
          <NavItem href='#'>Sign Up</NavItem>
        <NavItem href="/cart">Cart</NavItem>
